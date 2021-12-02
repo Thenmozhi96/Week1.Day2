c91fb93d482d905a4bbee9a7cde2bca3154a1500
Thenmozhi96 <95162211+Thenmozhi96@users.noreply.github.com>
2021-12-02 13:23:20 +0530

    Add files via upload

--- PrintDuplicatesInArray.java
+++ PrintDuplicatesInArray.java
@@ -6,20 +6,18 @@ public class PrintDuplicatesInArray {
 		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
 		//int len=arr.length;
 		
-		for(int i=0;i<arr.length;i++) 
+		for(int i=0;i<arr.length-1;i++) 
 		{
 			int count=0;
-					for(int j=0;j<arr.length;j++)
+					for(int j=i+1;j<arr.length;j++)
 					{
 						if(arr[i]==arr[j])
 						{
 							count=count+1;
+							System.out.println("Duplicate number is "+arr[i]);
 						}
 					}
-		if(arr[0]==arr[i])
-		{
-			System.out.println(arr[i]+" having "+count);
-		}
+				
 	}
 	
 	}
